0858897943544388a56d30e875e96911f86d6130
Carlos <same.carlosartiles@gmail.com>
2026-08-26 16:49:29 -0400

    Initial commit: Employee Management System Backend

---
+++ src/main/java/net/javaguides/ems_backend/mapper/EmployeeMapper.java
@@ -0,0 +1,31 @@
+package net.javaguides.ems_backend.mapper;
+
+import net.javaguides.ems_backend.dto.EmployeeDto;
+import net.javaguides.ems_backend.entity.Employee;
+
+public class EmployeeMapper {
+
+    public static EmployeeDto mapToEmployeeDto(Employee employee) {
+        Long departmentId = null;
+
+        if (employee.getDepartment() != null) {
+            departmentId = employee.getDepartment().getId();
+        }
+
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail(),
+                departmentId
+        );
+    }
+    public static Employee mapToEmployee (EmployeeDto employeeDto){
+            Employee employee = new Employee();
+                    employee.setId(employeeDto.getId());
+                    employee.setFirstName(employeeDto.getFirstName());
+                    employee.setLastName(employeeDto.getLastName());
+                    employee.setEmail(employeeDto.getEmail());
+                    return employee;
+    }
+}
---
+++ src/main/java/net/javaguides/ems_backend/service/impl/DepartmentServiceImpl.java
@@ -0,0 +1,67 @@
+package net.javaguides.ems_backend.service.impl;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.ems_backend.dto.DepartmentDto;
+import net.javaguides.ems_backend.entity.Department;
+import net.javaguides.ems_backend.exception.ResourceNotFoundException;
+import net.javaguides.ems_backend.mapper.DepartmentMapper;
+import net.javaguides.ems_backend.repository.DepartmentRepository;
+import net.javaguides.ems_backend.service.DepartmentService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class DepartmentServiceImpl implements DepartmentService {
+
+    private DepartmentRepository departmentRepository;
+
+    @Override
+    public DepartmentDto createDepartment(DepartmentDto departmentDto) {
+
+        Department department = DepartmentMapper.mapToDepartment(departmentDto);
+        Department savedDepartment = departmentRepository.save(department);
+        return DepartmentMapper.mapToDepartmentDto(savedDepartment);
+    }
+
+    @Override
+    public DepartmentDto getDepartmentById(Long departmentId) {
+        Department department = departmentRepository.findById(departmentId).orElseThrow(
+                () -> new ResourceNotFoundException("Department is not exist with a given id: " + departmentId)
+        );
+        return DepartmentMapper.mapToDepartmentDto(department);
+    }
+
+    @Override
+    public List<DepartmentDto> getAllDepartments() {
+        List<Department> departments = departmentRepository.findAll();
+        return departments.stream().map(DepartmentMapper::mapToDepartmentDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public DepartmentDto updateDepartment(Long departmentId, DepartmentDto updateDepartment) {
+
+        Department department = departmentRepository.findById(departmentId).orElseThrow(
+                () -> new ResourceNotFoundException("Department is not exists with a given id: " + departmentId)
+        );
+
+        department.setDepartmentName(updateDepartment.getDepartmentName());
+        department.setDepartmentDescription(updateDepartment.getDepartmentDescription());
+
+        Department savedDepartment = departmentRepository.save(department);
+
+        return DepartmentMapper.mapToDepartmentDto(savedDepartment);
+    }
+
+    @Override
+    public void deleteDepartment(Long departmentId) {
+        departmentRepository.findById(departmentId).orElseThrow(
+                () -> new ResourceNotFoundException("Department is not exists with a given id: " + departmentId)
+        );
+
+        departmentRepository.deleteById(departmentId);
+    }
+}
